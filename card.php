9c40d6aaf728786b977604a6f7039d11c2e6ed89
VIctor Toriz <20020300@es.uveg.edu.mx>
2022-06-14 15:27:42 -0500

    Se suben  cambios
    Se suben cambios. index.php y card.php

---
+++ card.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Exitoso</title>
+</head>
+<body>
+    <div class="contenido">
+    <div class="col1">
+Nombre  <?php echo $_POST["nombre"]?>
+Apellidos  <?php echo $_POST["apellidos"]?>
+Edad <?php echo $_POST["edad"]?>
+Correo <?php echo $_POST["email"]?>
+Contraseña <?php echo $_POST["password"]?>
+Nombre  <?php echo $_POST["fname"]?>
+Apellidos  <?php echo $_POST["lname"]?>
+</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="./styles.css" rel="sylesheet"></link>
+    <title>Uveg  Victor Toriz</title>
+</head>
+<body>
+    <form  method="GET" action ="card.php">
+        <div>
+        <input type="text" name="nombre" placeholder="Nombre" />
+        <input type="text" name="apellidos" placeholder="Apellidos" />
+        <input type="number" name="edad" placeholder="Edad" inputmode="numeric" />
+        <input type="email" name="email" placeholder="Correo" />
+        <input type="password" name="password" placeholder="Contraseña" inputmode="numeric" />
+        <button class="button">Registrar</button>
+        </div>
+    </form>
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</html>
