ea4d405813ebdd367eac29e2b41027b16442b253
VIctor Toriz <20020300@es.uveg.edu.mx>
2022-06-14 15:49:32 -0500

    Cambios finales
    Cambios finales index y card php

--- card.php
+++ card.php
@@ -1,22 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro Exitoso</title>
+    <!--SCRIPTS CSS BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./icon_a16c7dd575b0634749d3f60bcdf78a68.ico" />
 </head>
+
 <body>
-    <div class="contenido">
-    <div class="col1">
-Nombre  <?php echo $_POST["nombre"]?>
-Apellidos  <?php echo $_POST["apellidos"]?>
-Edad <?php echo $_POST["edad"]?>
-Correo <?php echo $_POST["email"]?>
-Contraseña <?php echo $_POST["password"]?>
-Nombre  <?php echo $_POST["fname"]?>
-Apellidos  <?php echo $_POST["lname"]?>
-</div>
-</div>
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5">
+                            <h2 class="text-center mb-4"><span style="color: rgb(51, 51, 51); background-color: rgb(255, 255, 255);">Registro exitoso!</span><br></h2>
+                            <div class="contenido">
+                                <div class="col1">
+                                    <br>
+                                    Nombre: <?php echo $_POST["nombre"] ?>
+                                    <br>
+                                    <br>Apellidos: <?php echo $_POST["apellidos"] ?>
+                                    <br>
+                                    <br>Edad: <?php echo $_POST["edad"] ?>
+                                    <br>
+                                    <br>Correo: <?php echo $_POST["email"] ?>
+                                    <br>
+                                    <br>Contraseña: <?php echo $_POST["password"] ?>
+                                    <br>
+                                </div>
+                            </div>
+                            <div>
+                                <br><input class="btn btn-secondary" onclick="history.back()" name="volver atrás" value="volver atrás">
+                            </div>
+
+                        </div>
+    </section>
 </body>
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </html>
--- index.php
+++ index.php
@@ -1,25 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link href="./styles.css" rel="sylesheet"></link>
-    <title>Uveg  Victor Toriz</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Uveg Victor Toriz</title>
+    <!--SCRIPTS CSS BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./icon_a16c7dd575b0634749d3f60bcdf78a68.ico" />
 </head>
+
 <body>
-    <form  method="GET" action ="card.php">
-        <div>
-        <input type="text" name="nombre" placeholder="Nombre" />
-        <input type="text" name="apellidos" placeholder="Apellidos" />
-        <input type="number" name="edad" placeholder="Edad" inputmode="numeric" />
-        <input type="email" name="email" placeholder="Correo" />
-        <input type="password" name="password" placeholder="Contraseña" inputmode="numeric" />
-        <button class="button">Registrar</button>
-        </div>
-    </form>
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5">
+                            <h2 class="text-center mb-4"><span style="color: rgb(51, 51, 51); background-color: rgb(255, 255, 255);">Registrar alumno</span><br></h2>
+                            <form method="POST" action="card.php">
+                                <input type="text" name="nombre" placeholder="Nombre" /><br>
+                                <small class="form-text text-muted">Coloca tu nombre.</small><br>
+                                <br><input type="text" name="apellidos" placeholder="Apellidos" /><br>
+                                <small class="form-text text-muted">Coloca tus apellidos.</small><br>
+                                <br><input type="number" name="edad" placeholder="Edad" inputmode="numeric" /><br>
+                                <small class="form-text text-muted">Coloca tu edad.</small><br>
+                                <br><input type="email" name="email" placeholder="Correo" inputmode="email" /><br>
+                                <small class="form-text text-muted">Coloca tu correo electronico.</small><br>
+                                <br><input type="password" name="password" placeholder="Contraseña" inputmode="numeric" /><br>
+                                <small class="form-text text-muted">Coloca tu contraseña.</small><br>
+                                <br><button class="btn btn-secondary">Registrar</button>
+                            </form>
+                        </div>
+    </section>
+</body>
 <!-- JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </html>
